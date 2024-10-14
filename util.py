84c04638cf4d7a115fd9d11e2ab01bb4cb0efe44
lukilme <kilmerluisluk12@gmail.com>
2024-10-14 11:53:04 -0300

    man, i lost

--- util.py
+++ util.py
@@ -22,41 +22,40 @@ def get_data_frames():
     data_dash = pd.read_csv("datasets/dash_sinusoid_8h.log", sep=",")
     return data_log, data_dash
 
+
 def get_data_concated(data_log, data_dash):
-  #removing all columns with std equal to 0.0, because they are constant 
-  #variables and did not help in creating the model
-  for colum in data_log.columns:
-      if(data_log[colum].std()==0.0):
-          data_log = data_log.drop(columns=[colum])
-      
-  #merging dataframes to correlate QoS results with telemetry
-  data_log = data_log.groupby("timestamp").mean().reset_index()
-  result_total = pd.concat(
-      [data_log.set_index("timestamp"), data_dash.set_index("timestamp")],
-      axis=1,
-      join="outer",
-  )
+    # Remover colunas constante
+    for colum in data_log.columns:
+        if(data_log[colum].std() == 0.0):
+            data_log = data_log.drop(columns=[colum])
 
-  #fill in the empty space with the column measurement
-  #removeSameValuedAttr
-  for index in result_total.columns:
-    media = result_total[index].mean()
-    result_total[index] = result_total[index].fillna(media)
-  result_total = result_total.reset_index()
+    # Agrupar por 'timestamp' e calcular a média
+    data_log = data_log.groupby("timestamp").mean().reset_index()
 
-  #drop the outliers
-  z_scores = np.abs(stats.zscore(result_total['framesDisplayedCalc']))
-  threshold = 3
-  result_total = result_total[(z_scores < threshold)]
+    # Concatenar os dataframes
+    result_total = pd.concat(
+        [data_log.set_index("timestamp"), data_dash.set_index("timestamp")],
+        axis=1,
+        join="outer"
+    )
+    # Preencher valores nulos com a média de cada coluna
+    result_total = result_total.fillna(result_total.mean())
 
-  #get the labels
-  labels = result_total['framesDisplayedCalc']
+    # Remover outliers com base no Z-score
+    z_scores = np.abs(stats.zscore(result_total['framesDisplayedCalc']))
+    threshold = 3
+    result_total = result_total[(z_scores < threshold)]
 
-  #and drop the rest
-  columns_to_remove = list(data_dash.columns)
-  columns_to_remove.remove('timestamp')
-  result_total = result_total.drop(columns=columns_to_remove)
-  return result_total, labels
+    # Extrair os labels
+    labels = result_total['framesDisplayedCalc']
+
+    # Remover colunas do data_dash
+    columns_to_remove = list(data_dash.columns)
+    columns_to_remove.remove('timestamp')
+    result_total = result_total.drop(columns=columns_to_remove)
+    
+    return result_total, labels
+
 
 def alert_end():
     from plyer import notification
